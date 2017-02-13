d0077ff1ea8d20fada77b75924551a1f099ceb06
Antonio Martins Miranda <amartmiranda@gmail.com>
2017-02-13 20:45:46 -0200

    acrescentando arquivo de saudacao

--- resources/views/saudacao/.#ola.blade.php
+++
@@ -1,1 +0,0 @@
-php.06@jerry.cec.ime.usp.br.22544:1486154911
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/saudacao/ola', function () {
-    return view('SaudacaoController@ola');
+// Página de saudação
+Route::get('/saudacao', function () {
+    return view('saudacao');
 });
